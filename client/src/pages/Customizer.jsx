2b2f61263f66af94cb4ffcba98fac1748cb1d35f
Nihar <wnihar04@gmail.com>
2025-05-08 18:07:30 +0530

    2nd commit

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -60,15 +60,18 @@ const Customizer = () => {
       setGeneratingImg(true);
       setErrorMessage("");
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://ai-and-threejs-website.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
